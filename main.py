8da3618cc1c4a60774d333b2c40d4d3d070dd5a0
Dakshkhandelwal1 <158986258+Dakshkhandelwal1@users.noreply.github.com>
2025-06-01 10:00:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,27 +69,27 @@ class mohakji(Agent):
         
         Maintain a warm, encouraging, and clear tone throughout the session. Emphasize conceptual clarity and step-by-step problem solving. Use the teaching style of a real classroom teacher who interacts actively with students.""")
 @function_tool()
-async def c2s(self) -> None:
+async def c2s() -> None:
     """Use this tool to handoff the call to mukesh ji."""
 
         # Perform a handoff, immediately transfering control to the new agent
     return mukeshji(chat_ctx=self.session.chat_ctx)
 @function_tool()
-async def c2m(self) -> None:
+async def c2m() -> None:
     """Use this tool to handoff the call to shrikantji."""
 
         # Perform a handoff, immediately transfering control to the new agent
     return shrikantji(chat_ctx=self.session.chat_ctx)
 
 @function_tool()
-async def c2ss(self) -> None:
+async def c2ss() -> None:
     """Use this tool to handoff the call to anitaji."""
 
         # Perform a handoff, immediately transfering control to the new agent
     return anitaji(chat_ctx=self.session.chat_ctx)
 
 @function_tool()
-async def c2sst(self) -> None:
+async def c2sst() -> None:
     """Use this tool to handoff the call to kanchanji."""
 
         # Perform a handoff, immediately transfering control to the new agent
@@ -97,7 +97,7 @@ async def c2sst(self) -> None:
 
 
 @function_tool()
-async def c2e(self) -> None:
+async def c2e() -> None:
     """Use this tool to handoff the call to manishji."""
 
         # Perform a handoff, immediately transfering control to the new agent
@@ -105,14 +105,14 @@ async def c2e(self) -> None:
 
 
 @function_tool()
-async def c2h(self) -> None:
+async def c2h() -> None:
     """Use this tool to handoff the call to Laxminarayanji."""
 
         # Perform a handoff, immediately transfering control to the new agent
     return laxminarayanji(chat_ctx=self.session.chat_ctx)
 
 @function_tool()
-async def c2cs(self) -> None:
+async def c2cs() -> None:
     """Use this tool to handoff the call to mohakji."""
 
         # Perform a handoff, immediately transfering control to the new agent
